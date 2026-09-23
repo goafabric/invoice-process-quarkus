48838d5047e393875c95a33699870ee735d66740
Andreas Mautsch <amautsch@gmx.de>
2026-09-23 16:40:27 +0000

    Update all-updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ val dockerRegistry = "goafabric"
 plugins {
 	java
 	jacoco
-	id("io.quarkus") version "3.39.4"
+	id("io.quarkus") version "3.39.5"
 	id("net.researchgate.release") version "3.1.0"
 	id("org.sonarqube") version "7.5.0.8588"
 
@@ -35,11 +35,11 @@ dependencies {
 
 		testImplementation("org.assertj:assertj-core:3.27.7")
 		testImplementation("com.tngtech.archunit:archunit-junit5:1.5.0")
-		testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
+		testImplementation("org.mockito.kotlin:mockito-kotlin:6.4.0")
 	}
 
-	kapt(enforcedPlatform("io.quarkus:quarkus-bom:3.39.4"))
-	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.39.4"))
+	kapt(enforcedPlatform("io.quarkus:quarkus-bom:3.40.0"))
+	implementation(enforcedPlatform("io.quarkus:quarkus-bom:3.40.0"))
 }
 dependencies {
 	//web
